ef5f16dac88a644a8ca0089201c5c88a46f69237
John West <john.sheffield.west@gmail.com>
2012-03-25 08:48:28 -0400

    something horribly wrong

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120321183930) do
+ActiveRecord::Schema.define(:version => 20120324011455) do
+
+  create_table "about_pages", :force => true do |t|
+    t.text     "text"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "agendas", :force => true do |t|
     t.string   "title"
@@ -20,6 +27,21 @@ ActiveRecord::Schema.define(:version => 20120321183930) do
   end
 
   create_table "articles", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "front_pages", :force => true do |t|
+    t.text     "text"
+    t.string   "title"
+    t.string   "subtitle"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pages", :force => true do |t|
+    t.text     "text"
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
